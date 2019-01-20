fc9f0d1da04c31f5073df8e8bdc406ef236a91c2
whbvac <wanghaibao@you>
2019-01-20 22:44:23 +0800

    init

---
+++ app/src/main/java/com/kotlin/whbvac/passwordkt/App.kt
@@ -0,0 +1,10 @@
+package com.kotlin.whbvac.passwordkt
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/kotlin/whbvac/passwordkt/ui/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.kotlin.whbvac.passwordkt.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId())
+        getIntentData()
+        initView()
+        initData()
+    }
+
+    abstract fun layoutId():Int
+
+    abstract fun getIntentData()
+
+    abstract fun initView()
+
+    abstract fun initData()
+}
